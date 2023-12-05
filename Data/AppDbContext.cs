70a0724bcb37aaee8aeb0906c638a7718263d88b
Gabriel Artemio <43358004+gabriel-artemio@users.noreply.github.com>
2023-12-05 13:28:12 -0300

    criação da model

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using MeuTodo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeuTodo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
---
+++ Models/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace MeuTodo.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public int Title { get; set; }
+        public bool Done { get; set; }
+    }
+}
