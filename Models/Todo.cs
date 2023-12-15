c8ac8ed560e8402d84fa2943e1fddc042cb12d2c
Gabriel Artemio <43358004+gabriel-artemio@users.noreply.github.com>
2023-12-15 12:23:22 -0300

    Delete Models directory

--- Models/Todo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MeuTodo.Models
-{
-    public class Todo
-    {
-        public int Id { get; set; }
-        public int Title { get; set; }
-        public bool Done { get; set; }
-    }
-}
