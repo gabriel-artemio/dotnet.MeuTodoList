17f2230eb2369c759db8f829c93187f8106b6329
Gabriel Artemio <43358004+gabriel-artemio@users.noreply.github.com>
2023-12-15 13:02:38 -0300

    add view model e controller

---
+++ ViewsModels/CreateTodoViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeuTodo.ViewsModels
+{
+    public class CreateTodoViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+    }
+}
